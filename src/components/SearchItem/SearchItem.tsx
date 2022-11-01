78874917f473df181f9eaf021c32dfc3359c22e1
Nguyen Duong <muoi07052001@gmail.com>
2022-11-01 11:00:12 +0700

    Add Hotel Detail

--- src/components/SearchItem/SearchItem.tsx
+++ src/components/SearchItem/SearchItem.tsx
@@ -10,21 +10,45 @@ const SearchItem = () => {
         className={styles['search-item__img']}
       />
       <div className={styles['search-item__description']}>
-        <h1 className={styles['search-item__description__title']}>Tower Street Apartments</h1>
-        <span className={styles['search-item__description__distance']}>500m from center</span>
-        <span className={styles['search-item__description__taxi-option']}>Free airport taxi</span>
+        <h1 className={styles['search-item__description__title']}>
+          Tower Street Apartments
+        </h1>
+        <span className={styles['search-item__description__distance']}>
+          500m from center
+        </span>
+        <span className={styles['search-item__description__taxi-option']}>
+          Free airport taxi
+        </span>
         <span className={styles['search-item__description__subtitle']}>
           Studio Apartment with Air conditioning
         </span>
         <span className={styles['search-item__description__feature']}>
           Entire studio • 1 bathroom • 21m² 1 full bed
         </span>
-        <span className={styles['search-item__description__cancel-option']}>Free cancellation </span>
-        <span className={styles['search-item__description__cancel-option-subtitle']}>
+        <span className={styles['search-item__description__cancel-option']}>
+          Free cancellation{' '}
+        </span>
+        <span
+          className={styles['search-item__description__cancel-option-subtitle']}
+        >
           You can cancel later, so lock in this great price today!
         </span>
       </div>
-      <div className={styles['search-item__detail']}>details</div>
+      <div className={styles['search-item__detail']}>
+        <div className={styles['search-item__detail__rating']}>
+          <span>Excellent</span>
+          <button>8.9</button>
+        </div>
+        <div className={styles['search-item__detail__text']}>
+          <span className={styles['search-item__detail__text__price']}>
+            $123
+          </span>
+          <span className={styles['search-item__detail__text__tax-option']}>
+            Include taxes and fees
+          </span>
+          <button className={styles['search-item__detail__text__check-btn']}>See availablity</button>
+        </div>
+      </div>
     </div>
   );
 };
