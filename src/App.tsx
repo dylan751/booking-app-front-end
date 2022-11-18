c08d7872e1af195c6c03040dc4cb0dfd5dbc12d9
Nguyen Duong <muoi07052001@gmail.com>
2022-11-18 22:38:28 +0700

    Add Loading Skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import 'react-loading-skeleton/dist/skeleton.css'
 import HomePage from './pages/HomePage';
 import HotelItem from './pages/HotelItem';
 import HotelList from './pages/HotelList';
--- src/components/Featured/index.tsx
+++ src/components/Featured/index.tsx
@@ -1,56 +1,56 @@
 import React from 'react';
 import useFetch from '../../hooks/useFetch';
 import { CountByCity } from '../../models/CountByCity';
+import FeaturedSkeleton from '../LoadingSkeleton/FeaturedSkeleton';
 import styles from './Featured.module.scss';
 
 const Featured = () => {
   const { data, loading, error } = useFetch<CountByCity[]>(
-    `${process.env.REACT_APP_API_ENDPOINT}/hotels/count/byCity?cities=berlin,madrid,london`,
+    `${process.env.REACT_APP_API_ENDPOINT}/hotels/count/byCity?cities=Krakow,Budapest,Madrid`,
   );
 
   if (error) {
     return <div>{error.message}</div>;
   }
 
+  const dummy_featured = [
+    {
+      src: 'https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o=',
+      city: 'Krakow',
+      propertyCount: data && data[0],
+    },
+    {
+      src: 'https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o=',
+      city: 'Budapest',
+      propertyCount: data && data[1],
+    },
+    {
+      src: 'https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o=',
+      city: 'Madrid',
+      propertyCount: data && data[2],
+    },
+  ];
+
   return (
     <div className={styles['featured']}>
       {loading ? (
-        'Loading Please wait'
+        // <FeaturedSkeleton />
+        'Loading Please wait...'
       ) : (
         <>
-          <div className={styles['featured__item']}>
-            <img
-              src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
-              alt=""
-              className={styles['featured__item__img']}
-            />
-            <div className={styles['featured__item__title']}>
-              <h1>Berlin</h1>
-              <h2>{data && data[0]} properties</h2>
+          {dummy_featured.map((featured, index) => (
+            <div className={styles['featured__item']} key={index}>
+              <img
+                src={featured.src}
+                alt=""
+                className={styles['featured__item__img']}
+              />
+              <div className={styles['featured__item__title']}>
+                <h1>{featured.city}</h1>
+                <h2>{featured.propertyCount} properties</h2>
+              </div>
             </div>
-          </div>
-          <div className={styles['featured__item']}>
-            <img
-              src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
-              alt=""
-              className={styles['featured__item__img']}
-            />
-            <div className={styles['featured__item__title']}>
-              <h1>Madrid</h1>
-              <h2>{data && data[1]} properties</h2>
-            </div>
-          </div>
-          <div className={styles['featured__item']}>
-            <img
-              src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
-              alt=""
-              className={styles['featured__item__img']}
-            />
-            <div className={styles['featured__item__title']}>
-              <h1>London</h1>
-              <h2>{data && data[2]} properties</h2>
-            </div>
-          </div>
+          ))}
         </>
       )}
     </div>
