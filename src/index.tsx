5eaf9bd9edd1ccd7a05a6e9f1867ef17fed3c82f
Nguyen Duong <muoi07052001@gmail.com>
2022-11-27 23:00:28 +0700

    Fix bug overflow-x

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './styles/global.scss';
 import { AuthContextProvider } from './context/AuthContext';
 import { ReserveContextProvider } from './context/ReserveContext';
 import { SearchContextProvider } from './context/SearchContext';
