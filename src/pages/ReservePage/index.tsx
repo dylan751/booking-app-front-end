f66befdf79fa8fa492362a3a76c73a16859cb523
Nguyen Duong <muoi07052001@gmail.com>
2022-11-28 16:42:02 +0700

    Fix lint

--- src/pages/ReservePage/index.tsx
+++ src/pages/ReservePage/index.tsx
@@ -3,27 +3,20 @@ import { useLocation } from 'react-router-dom';
 import Navbar from '../../components/Navbar';
 import { AuthContext } from '../../context/AuthContext';
 import { ReserveContext } from '../../context/ReserveContext';
-import { SearchContext } from '../../context/SearchContext';
 import useFetch from '../../hooks/useFetch';
 import { Form } from '../../models/Form';
 import { Hotel } from '../../models/Hotel';
 import { Room } from '../../models/Room';
-import { dayDifference } from '../../services/utils';
 import ReserveDetails from './ReserveDetails';
 import ReserveFinalStep from './ReserveFinalStep';
 import styles from './ReservePage.module.scss';
 
 const ReservePage = () => {
-  const { dates, options } = useContext(SearchContext);
   const [step, setStep] = useState(1);
   const location = useLocation();
   const hotelId = location.pathname.split('/')[2];
 
-  const {
-    data: hotelData,
-    loading,
-    error,
-  } = useFetch<Hotel>(
+  const { data: hotelData } = useFetch<Hotel>(
     `${process.env.REACT_APP_API_ENDPOINT}/hotels/${hotelId}`,
   );
 
@@ -34,10 +27,6 @@ const ReservePage = () => {
       process.env.REACT_APP_API_ENDPOINT
     }/rooms/multiple/${selectedRooms.toString()}`,
   );
-
-  const numberOfDays = dayDifference(dates[0].startDate, dates[0].endDate);
-  const price =
-    hotelData && numberOfDays * hotelData.cheapestPrice * options.room;
 
   const [formData, setFormData] = useState<Form>({
     isTravelForWork: false,
